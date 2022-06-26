4947e413ac69381fe9414d23fb5e6c0d651b04fe
Muhammed Abdrahman <muhammed03amin.com>
2022-06-27 02:37:45 +0600

    refactor: clean up code

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
-import { httpServer } from './http-server';
+import { httpServer } from './src/http-server';
 import { WebSocketServer, WebSocket, createWebSocketStream } from 'ws';
-import { handleCommand } from './handlers';
+import { handleCommand } from './src/handlers';
+
+const HTTP_PORT = process.env.HTTP_PORT || 3000;
+const WS_PORT = process.env.WS_PORT || 8080;
 
 console.log(`Start static http server on the 3000 port!`);
-httpServer.listen(3000);
+httpServer.listen(HTTP_PORT);
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: +WS_PORT });
 
 const socketArray: WebSocket[] = [];
 
@@ -16,12 +19,14 @@ wss.on('connection', (ws: WebSocket) => {
   wsStream.on('data', async (data) => {
     console.log('received: %s', data);
 
-    const [command, ...option] = data.toString().split(' ');
+    try {
+      const [command, ...option] = data.toString().split(' ');
 
-    const sendOptionString = await handleCommand(command, option);
-    const sendString = `${command} ${sendOptionString}\0`;
+      const sendOptionString = await handleCommand(command, option);
+      const sendString = `${command} ${sendOptionString}\0`;
 
-    wsStream.write(sendString);
+      wsStream.write(sendString);
+    } catch  {}
   });
 });
 
